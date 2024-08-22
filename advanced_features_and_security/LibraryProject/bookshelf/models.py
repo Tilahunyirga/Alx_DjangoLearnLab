28099d8b535bc5b891f77a02cc0cb74440b5f42d
tilahun <tilahunyirga48@gmail.com>
2024-08-22 13:15:56 +0300

    superuser

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager, AbstractBaseUser
 # Create your models here.
 class CustomUser(AbstractUser):
   date_of_birth = models.DateField
   profile_photo =models.ImageField(width_field=10, blank=True)
   
+  
+class CustomUserManager(BaseUserManager):
+  def create_user():
+    pass 
+  
+
+  def create_superuser():
+    pass
   
