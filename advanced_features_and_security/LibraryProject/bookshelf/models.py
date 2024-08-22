6ad791e42c8f827b39fa803147557c0ae79d90c1
tilahun <tilahunyirga48@gmail.com>
2024-08-22 13:09:18 +0300

    add

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
-
-class user(AbstractUser):
-  date_of_birth = models.DateField
-  profile_photo =models.ImageField(width_field=10, blank=True)
+  
+class CustomUser(AbstractUser):
+  pass
