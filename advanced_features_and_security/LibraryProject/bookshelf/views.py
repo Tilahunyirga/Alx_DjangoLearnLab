0af34b9254d8b3018a3072ff3a97e18f8f2ed752
tilahun <tilahunyirga48@gmail.com>
2024-08-23 14:06:30 +0300

    =-=-=

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth import login 
+from django.contrib.auth.models import Group
+from .models import post 
+
+def book_list(request):
+  book_list = book_list.objects.all()
+  return render(request, "bookshelf/book_list.html", {"posts":book_list} )
 
 # Create your views here.
