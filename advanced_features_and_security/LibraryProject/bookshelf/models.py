e4ba83053116c3fd48d713a80f35028663082789
tilahun <tilahunyirga48@gmail.com>
2024-08-23 13:33:46 +0300

    ==

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -13,7 +13,11 @@ class CustomUser(AbstractUser):
   
 class book(models.Model):
   title =models.CharField(max_length=100),
-  author = models.CharField(max_length=100)   
+  author = models.CharField(max_length=100) 
+
+class post(models.Model):
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, )
+  title = models.CharField(max_length=100)    
   class meta:
     Permissions =[
       ("can_view_Book","can view book" ),
