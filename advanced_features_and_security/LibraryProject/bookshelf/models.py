fc3a5b3b6f8e41847407052b0cc612bbeb2d099c
tilahun <tilahunyirga48@gmail.com>
2024-08-23 13:12:54 +0300

    task 1 permmisions

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import Permission
+
+
+
+
 # Create your models here.
 class CustomUser(AbstractUser):
   date_of_birth = models.DateField
   profile_photo =models.ImageField(width_field=10, blank=True)
+  
+  class meta:
+    Permissions =[
+      ("can_view","can view" ),
+      ("can_create", "can create"),
+      ("can_edit", "can edit"),
+      ("can_delete", "can delete")
+      
+    ]
   
   
 class CustomUserManager(BaseUserManager):
@@ -13,4 +27,5 @@ class CustomUserManager(BaseUserManager):
 
   def create_superuser():
     pass
+  
   
