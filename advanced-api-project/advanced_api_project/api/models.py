346b8ba7e7907f7841289379a97b2dc580a4a1e4
tilahun <tilahunyirga48@gmail.com>
2024-09-07 18:54:40 +0300

    Remove all files and directories

--- advanced-api-project/advanced_api_project/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Author(models.Model):
-  name = models.CharField(max_length=100)
-
-class Book(models.Model):
-  title = models.CharField(max_length=100)
-  publication_year = models.IntegerField
-  author = models.ForeignKey(Author, related_name='book', on_delete=models.CASCADE)
