cd9ed537832d0536797e454d6d6cc2ace72151b5
tilahun <tilahunyirga48@gmail.com>
2024-09-07 22:03:32 +0300

    serializer added

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -11,13 +11,13 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
-  authors = AuthorSerializer(many=True, read_only =True)
+  author = serializers.CharField(many=True, read_only =True)
   class Meta:
     model = Book
     fields ='__all__'
     
     def validate(self, data):
-      if data['publication_year']> datetime.date.today():
+      if data['publication_year'] > datetime.date.today():
         raise serializers.ValidationError("not found")
     
     
