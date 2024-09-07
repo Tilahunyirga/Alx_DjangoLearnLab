7032cc56db1f4a2858a8e74a8abb7d1a76b266fb
tilahun <tilahunyirga48@gmail.com>
2024-09-07 18:24:28 +0300

    advanced api task 0

---
+++ advanced_api_project/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Book
+from .models import Author
+import datetime
+
+class AuthorSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Author
+    fields = ['id', 'name']   
+  
+
+
+class BookSerializer(serializers.ModelSerializer):
+  authors = AuthorSerializer(many=True, read_only =True)
+  class Meta:
+    model = Book
+    fields ='__all__'
+    
+    def validate(self, data):
+      if data['publication_year']> datetime.date.today():
+        raise serializers.ValidationError("not found")
+    
+    
