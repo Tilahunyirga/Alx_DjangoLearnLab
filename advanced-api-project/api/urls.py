f9197bd2e7fd68326a7fed200325e1247eae19d4
tilahun <tilahunyirga48@gmail.com>
2024-09-08 10:09:00 +0300

    task 1 views added

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
   path('books/', BookListView.as_view(), name='book_list'),
   path('books/create/', BookCreateView.as_view(), name='book_create'),
   path('books/update', BookUpdateView.as_view(), name='book_update' ),
-  path('books/detail/',BookDetailView.as_view(), name='book_retrieve')
+  path('books/detail/',BookDetailView.as_view(), name='book_retrieve'),
+  path('books/delete', )
 
 ]
