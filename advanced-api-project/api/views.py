b9b5a0b39fb3f391f8a1c9a9fc52089056ecd49d
tilahun <tilahunyirga48@gmail.com>
2024-09-08 10:50:43 +0300

    task 2 add

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -9,6 +9,7 @@ from rest_framework.generics import (
 from rest_framework import generics
 from.models import Book
 from.serializers import BookSerializer
+from rest_framework import filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
