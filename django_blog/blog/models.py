6c38760ff2b65c732ca3c77966065ae46fcd3b9d
tilahun <tilahunyirga48@gmail.com>
2024-09-15 09:45:41 +0300

    task 0 fixing

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  author= models.CharField(max_length=100)
+  
+class Post(models.Model):
+  title = models.CharField(max_length=200)
+  content = models.TextField()
+  published_date = models.DateTimeField(auto_now_add=True)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  
