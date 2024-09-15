357837ebf4c326ab902193d4e7836c3549d3ea28
tilahun <tilahunyirga48@gmail.com>
2024-09-15 22:00:40 +0300

    task 2 added again

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -12,6 +12,11 @@ from django.views.generic import (ListView, CreateView,
                      UpdateView )
 from django.views.generic.detail import DetailView
 from .serializers import PostSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from requests import Response 
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 
@@ -24,17 +29,36 @@ class PostDetailView(DetailView):
   queryset = Post.objects.get('id')
   serializer_class = PostSerializer
   
-class PostCreateView(CreateView):
-  queryset = Post.objects.all()
-  serializer_class = PostSerializer
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
   
-class PostUpdateView(UpdateView):
-  queryset = Post.objects.all()
-  serializer_class = PostSerializer   
-  
-class PostDeleteView(DeleteView):
-  queryset = Post.objects.all()
-  serializer_class = PostSerializer     
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post-list')
+    template_name = 'blog/post_confirm_delete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author     
 
 
 def register(request):
@@ -54,6 +78,3 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def profile(request):
     return render(request, 'blog/profile.html')
-
-
-class 
