6b8eeab9624ba46be8aceaddcc4a44dfe94225b2
tilahun <tilahunyirga48@gmail.com>
2024-09-15 12:31:24 +0300

    dd

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from rest_framework import serializers
 class Post(models.Model):
   title = models.CharField(max_length=200)
   content = models.TextField()
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,33 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+
+
+
+
+class PostListAPIView(generics.ListAPIView):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
+  
+class PostDetailAPIView(generics.ListAPIViewAPIView):
+  queryset = Post.objects.get('id')
+  serializer_class = PostSerializer
+  
+class PostCreateAPIView(generics.CreateAPIView):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
+  
+class PostUpdateAPIView(generics.UpdateAPIView):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer   
+  
+class PostDeleteAPIView(generics.DestroyAPIView):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer     
+
 
 def register(request):
     if request.method == 'POST':
@@ -22,3 +49,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def profile(request):
     return render(request, 'blog/profile.html')
+
+
+class 
