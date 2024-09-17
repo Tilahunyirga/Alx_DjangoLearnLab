c00deb7758fb4264c4ba83a9a55403bef0cffed2
tilahun <tilahunyirga48@gmail.com>
2024-09-17 19:08:11 +0300

    deploying django

--- install/Scripts/activate.bat
+++ install/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Habtam\Documents\week13\django_blog\install
+set VIRTUAL_ENV=C:\Users\Habtam\Documents\week13\install
 
 if not defined PROMPT set PROMPT=$P$G
 
