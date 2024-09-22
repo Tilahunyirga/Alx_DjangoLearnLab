ec8e9de228799d2fe3c756a4ebbbc861afb7878f
tilahun <tilahunyirga48@gmail.com>
2024-09-22 13:16:23 +0300

    dg

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,7 +10,9 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+      Token.objects.create
+      user = CustomUser.objects.create_user(**validated_data)
+      get_user_model().objects.create_use
         return user
 
 
