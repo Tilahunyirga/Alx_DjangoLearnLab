8fcdf080f7ebdaacd52e96e1cc69522a9de74cb2
tilahun <tilahunyirga48@gmail.com>
2024-09-22 17:38:21 +0300

    task 2 again

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import generics, status
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
